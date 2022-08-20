79940458f615f9c0a11495bf46602140f2a25d2c
Vincent Jiang <vincejiang1996@gmail.com>
2022-08-20 08:33:04 +0000

    Add environmental variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const dotenv = require("dotenv").config({
-  path: path.join(__dirname, ".env.local"),
+  path: path.join(__dirname, ".env"),
 });
 
 module.exports = {
